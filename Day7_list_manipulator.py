bfe6901219b7fa252eaeed0b505f674ae8d681ff
Nouchie <167220040+Nouchie@users.noreply.github.com>
2024-07-11 01:30:48 +0000

    day 7

---
+++ Day7_list_manipulator.py
@@ -0,0 +1,19 @@
+def add_to_list(L: list, target):
+    l = L.copy()
+    if target not in l:
+        l.append(target)
+    return l
+
+def remove_from_list(L: list, target):
+    l = L.copy()
+    if target in l:
+        l.remove(target)
+    return l
+
+def sort_items(L: list):
+    l = L.copy()
+    l.sort()
+    return l
+
+def main():
+    
